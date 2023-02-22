02aeee8dd630b57185184707e2f2f4118d1c20cf
Gabriel Nagy <gabriel.nagy@canonical.com>
2023-02-22 12:18:26 +0200

    Allow configuration of environment system proxy
    The actual application of the environment proxy settings is quite
    simple, the more interesting part is the abstraction of the proxy
    settings so we can easily apply them to other backends in the
    future (like APT and GSettings).
    
    We validate all proxy settings to ensure they're in the right format, we
    properly escape them while ensuring we don't escape the already escaped
    ones, then we can represent them in the required format which in this
    case is as environment variables.
    
    DEENG-553

---
+++ internal/proxy/env.go
@@ -0,0 +1,72 @@
+package proxy
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/ubuntu/decorate"
+)
+
+// envString formats a proxy setting to be environment variable compliant.
+func (p setting) envString() string {
+	// Return both uppercase and lowercase environment variables for
+	// compatibility with different tools
+	return fmt.Sprintf("%s_PROXY=%s\n%s_proxy=%s\n",
+		strings.ToUpper(fmt.Sprint(p.protocol)), p.escapedURL,
+		strings.ToLower(fmt.Sprint(p.protocol)), p.escapedURL)
+}
+
+// applyToEnvironment applies the proxy configuration in the form of
+// environment variables set in /etc/environment.d.
+// If there are no proxy settings to apply, the environment file is removed.
+func (p Proxy) applyToEnvironment() (err error) {
+	defer decorate.OnError(&err, "couldn't apply environment proxy configuration")
+
+	if len(p.settings) == 0 {
+		log.Debug("No proxy settings to apply, removing environment file if it exists")
+		if err := os.Remove(p.envConfigPath); err != nil && !errors.Is(err, fs.ErrNotExist) {
+			return err
+		}
+		return nil
+	}
+
+	log.Debugf("Applying environment proxy configuration to %q", p.envConfigPath)
+	content := fmt.Sprintln(confHeader)
+	for _, p := range p.settings {
+		content += p.envString()
+	}
+
+	if prevContent, err := previousConfig(p.envConfigPath); err == nil && prevContent == content {
+		log.Debugf("Environment proxy configuration at %q is already up to date", p.envConfigPath)
+		return nil
+	} else if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return err
+	}
+
+	// Check if the parent directory exists - attempt to create the structure if not
+	envConfigDir := filepath.Dir(p.envConfigPath)
+	if _, err := os.Stat(envConfigDir); errors.Is(err, fs.ErrNotExist) {
+		log.Debugf("Creating directory %q", envConfigDir)
+		// #nosec G301 - /etc/environment.d permissions are 0755, so we should keep the same pattern
+		if err := os.MkdirAll(envConfigDir, 0755); err != nil {
+			return fmt.Errorf("failed to create environment config parent directory: %w", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("unexpected error while checking environment directory: %w", err)
+	}
+
+	// #nosec G306 - /etc/environment.d/* permissions are 0644, so we should keep the same pattern
+	if err := os.WriteFile(p.envConfigPath+".new", []byte(content), 0644); err != nil {
+		return err
+	}
+	if err := os.Rename(p.envConfigPath+".new", p.envConfigPath); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/proxy/setting.go
@@ -0,0 +1,150 @@
+package proxy
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/ubuntu/decorate"
+	"golang.org/x/exp/slices"
+)
+
+//go:generate stringer -type=protocol -trimprefix=protocol
+type protocol uint8
+
+const (
+	protocolAll protocol = iota
+	protocolNo
+	protocolHTTP
+	protocolHTTPS
+	protocolFTP
+	protocolSOCKS
+)
+
+// setting represents a proxy setting to be applied on the system.
+type setting struct {
+	protocol   protocol
+	escapedURL string // scheme://host:port, including escaped user:password if available, verbatim if no_proxy
+
+	url *url.URL
+}
+
+// newSettings parses and validates the given proxy settings, returning them in a
+// format ready to be applied on the system.
+func newSettings(http, https, ftp, socks, noproxy string) (settings []setting, err error) {
+	defer decorate.OnError(&err, "couldn't set proxy configuration")
+
+	if http != "" {
+		setting, err := newSetting(protocolHTTP, http)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	if https != "" {
+		setting, err := newSetting(protocolHTTPS, https)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	if ftp != "" {
+		setting, err := newSetting(protocolFTP, ftp)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	if socks != "" {
+		setting, err := newSetting(protocolSOCKS, socks)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	// If all protocols are set, and the escaped values are the same, we can safely set all_proxy as well
+	if len(settings) == 4 && slices.EqualFunc(settings, settings, func(a, _ setting) bool { return a.escapedURL == settings[0].escapedURL }) {
+		setting, err := newSetting(protocolAll, http)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	if noproxy != "" {
+		setting, err := newSetting(protocolNo, noproxy)
+		if err != nil {
+			return nil, err
+		}
+		settings = append(settings, setting)
+	}
+
+	return settings, nil
+}
+
+// newSetting creates a new proxy setting from the given protocol and URL.
+// It returns an error if the URL is invalid.
+func newSetting(proto protocol, uri string) (p setting, err error) {
+	defer decorate.OnError(&err, "couldn't create proxy setting")
+
+	// noProxy is a special case and we don't need to parse it
+	if proto == protocolNo {
+		return setting{protocol: proto, escapedURL: uri}, nil
+	}
+	// Ideally we would've handled this after calling url.Parse, by checking the
+	// Scheme attribute, but it's not reliable in case we parse an URI like
+	// "example.com:8000" and "example.com" is treated as a scheme because of
+	// the colon in the URI.
+	if !strings.Contains(uri, "://") {
+		return p, fmt.Errorf("missing scheme in proxy URI %q", uri)
+	}
+
+	uri = escapeURLCredentials(uri)
+	parsedURL, err := url.Parse(uri)
+	if err != nil {
+		return p, err
+	}
+
+	var host string
+	if parsedURL.User != nil {
+		host = parsedURL.User.String() + "@"
+	}
+	host += parsedURL.Host
+	escapedURL := fmt.Sprintf("%s://%s", parsedURL.Scheme, host)
+
+	return setting{
+		escapedURL: escapedURL,
+		protocol:   proto,
+		url:        parsedURL,
+	}, nil
+}
+
+// escapeURLCredentials escapes special characters from the credentials in the
+// given URL, if any.
+func escapeURLCredentials(uri string) string {
+	// Attempt to unescape the string first, discarding any error
+	// At best, this prevents us from escaping the URL multiple times
+	// At worst, the URL is not affected (we will treat % signs as part of the
+	// credentials and escape them later)
+	uri, _ = url.PathUnescape(uri)
+
+	// Regexp to check if the URI contains credentials
+	r := regexp.MustCompile(`^\w+://(?:(?P<credentials>.*:?.*)@)[a-zA-Z0-9.-]+(:[0-9]+)?/?$`)
+	matchIndex := r.SubexpIndex("credentials")
+	matches := r.FindStringSubmatch(uri)
+	if len(matches) >= matchIndex {
+		creds := matches[matchIndex]
+		user, password, found := strings.Cut(creds, ":")
+		if found {
+			return strings.Replace(uri, creds, url.UserPassword(user, password).String(), 1)
+		}
+		return strings.Replace(uri, creds, url.PathEscape(user), 1)
+	}
+
+	return uri
+}
